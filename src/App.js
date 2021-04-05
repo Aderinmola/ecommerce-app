fc840894f0182fa9019d3fed76c3f92b80330455
Aderinmola <favorsurpisesO1@gmail.com>
2021-04-05 12:56:18 -0700

    Setup the Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-// import logo from './logo.svg';
-// import './App.css';
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
+
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
     </div>
   );
 }
