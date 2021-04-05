bf1c5717d5d977c39089918d1ebd83019e23eb40
Aderinmola <favorsurpisesO1@gmail.com>
2021-04-05 16:51:44 -0700

    Added Footer Component to the Home Page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './styles.scss';
-import '../../default.scss';
-import Logo from './../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
+import Logo from './../../assets/logo.png';
 
 const Header = props => {
   return (
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="LOGO" alt="LOGO" />
+          <Link to="/">
+            <img src={Logo} alt="LOGO" />
+          </Link>
+        </div>
+
+        <div className="callToActions">
+          <ul>
+            <li>
+              <Link to="/registration">
+                Register
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
