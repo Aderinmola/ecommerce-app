ada8fd0271ce5a3c7c58eb2c2aa9cdb1d63f2792
Aderinmola <favorsurpisesO1@gmail.com>
2021-04-08 01:12:28 +0100

    Finished up with Google Signin

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyALbhWzGPVbcyW3nFJA-jZt_XF9o_cWV50",
+  authDomain: "ecommerce-ac50c.firebaseapp.com",
+  projectId: "ecommerce-ac50c",
+  storageBucket: "ecommerce-ac50c.appspot.com",
+  messagingSenderId: "517668232267",
+  appId: "1:517668232267:web:489aedb440fc2e0b7001ce"
+};
